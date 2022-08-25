8507fce04a331ebc1627b287223f56ae16309d65
Dipankar <dipankar15-11137@diu.edu.bd>
2022-08-25 14:40:22 +0600

    add banner

---
+++ src/Components/Pages/Home/Banner.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+       <div className='p-5'>
+         <div class="carousel carousel-center rounded-box">
+  <div class="carousel-item">
+    <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+  </div> 
+  <div class="carousel-item">
+    <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+  </div> 
+  <div class="carousel-item">
+    <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+  </div> 
+  <div class="carousel-item">
+    <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+  </div> 
+  <div class="carousel-item">
+    <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+  </div> 
+  <div class="carousel-item">
+    <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+  </div> 
+  <div class="carousel-item">
+    <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+  </div>
+</div>
+       </div>
+    );
+};
+
+export default Banner;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div className="bg-backgroundColor text-white">
-      <h1>Home</h1>
+      <Banner/>
     </div>
   );
 };
