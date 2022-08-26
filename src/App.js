fd9053c8806cb29becd63b23d99128f8caf74580
Dipankar <dipankar15-11137@diu.edu.bd>
2022-08-26 11:55:23 +0600

    add login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./Components/Login/Login";
+import Blogs from "./Components/Pages/Blogs/Blogs";
 import Home from "./Components/Pages/Home/Home";
 import Header from "./Components/Pages/Share/Header";
 import Navbar from "./Components/Pages/Share/Navbar";
@@ -6,9 +9,16 @@ import Navbar from "./Components/Pages/Share/Navbar";
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      {/* <Login /> */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
