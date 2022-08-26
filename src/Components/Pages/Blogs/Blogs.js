d2a23748438d37b6afda24f979af453264a9cc5d
Dipankar <dipankar15-11137@diu.edu.bd>
2022-08-26 12:24:33 +0600

    requireAuth

--- src/Components/Pages/Blogs/Blogs.js
+++ src/Components/Pages/Blogs/Blogs.js
@@ -2,13 +2,8 @@ import React from "react";
 
 const Blogs = () => {
   return (
-    <div>
+    <div className="text-white">
       <h1>Blogs</h1>
-      <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img src="https://placeimg.com/80/80/people" />
-        </div>
-      </label>
     </div>
   );
 };
